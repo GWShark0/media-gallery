7ba5556ca894030c0b4179483c68bd7f65643c8d
Greg Douglas <gwshark0@gmail.com>
2019-07-22 10:21:58 -0400

    Skip API call on empty submission

--- src/util/fetchData.js
+++ src/util/fetchData.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 // cue Dave Brubeck
 async function fetchData(query, callback) {
+  if (!query) {
+    return callback([]);
+  }
   const { data } = await axios(`/search/photos?query=${query}`);
   callback(data);
 }
